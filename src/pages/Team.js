8afa508c873651f92d89f0fa23e5cf52661b6c99
Caroline Van <cmvan@stanford.edu>
2022-10-05 17:37:54 -0700

    fix team code syntax

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -15,240 +15,240 @@ export default function Team() {
       <h1>This is the Team Page</h1>
       <p>Click on Each Member to Learn More!</p>
       <Row xs={1} md={2} lg={4} className="g-4 team-row">
+        <Col>
+        <Card onClick={() => {setModalShow(true)}} className="team-card">
+          <Card.Img variant="top" src={ dummyHeadshot } />
+          <Card.Body>
+            <Card.Title>Member Name</Card.Title>
+            <Card.Text>
+              Superhero Name or some catchphrase
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Modal
+          show={modalShow}
+          onHide={() => setModalShow(false)}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+              Member Name
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>
+              Member Bio
+            </p>
+          </Modal.Body>
+        </Modal>
+      </Col>
       <Col>
-      <Card onClick={() => {setModalShow(true)}} className="team-card">
-        <Card.Img variant="top" src={ dummyHeadshot } />
-        <Card.Body>
-          <Card.Title>Member Name</Card.Title>
-          <Card.Text>
-            Superhero Name or some catchphrase
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Modal
-        show={modalShow}
-        onHide={() => setModalShow(false)}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            Member Name
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <p>
-            Member Bio
-          </p>
-        </Modal.Body>
-      </Modal>
-    </Col>
-    <Col>
-      <Card onClick={() => {setModalShow(true)}} className="team-card">
-        <Card.Img variant="top" src={ dummyHeadshot } />
-        <Card.Body>
-          <Card.Title>Member Name</Card.Title>
-          <Card.Text>
-            Superhero Name or some catchphrase
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Modal
-        show={modalShow}
-        onHide={() => setModalShow(false)}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            Member Name
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <p>
-            Member Bio
-          </p>
-        </Modal.Body>
-      </Modal>
-    </Col>
-    <Col>
-      <Card onClick={() => {setModalShow(true)}} className="team-card">
-        <Card.Img variant="top" src={ dummyHeadshot } />
-        <Card.Body>
-          <Card.Title>Member Name</Card.Title>
-          <Card.Text>
-            Superhero Name or some catchphrase
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Modal
-        show={modalShow}
-        onHide={() => setModalShow(false)}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            Member Name
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <p>
-            Member Bio
-          </p>
-        </Modal.Body>
-      </Modal>
-    </Col>
-    <Col>
-      <Card onClick={() => {setModalShow(true)}} className="team-card">
-        <Card.Img variant="top" src={ dummyHeadshot } />
-        <Card.Body>
-          <Card.Title>Member Name</Card.Title>
-          <Card.Text>
-            Superhero Name or some catchphrase
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Modal
-        show={modalShow}
-        onHide={() => setModalShow(false)}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            Member Name
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <p>
-            Member Bio
-          </p>
-        </Modal.Body>
-      </Modal>
-    </Col>
+        <Card onClick={() => {setModalShow(true)}} className="team-card">
+          <Card.Img variant="top" src={ dummyHeadshot } />
+          <Card.Body>
+            <Card.Title>Member Name</Card.Title>
+            <Card.Text>
+              Superhero Name or some catchphrase
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Modal
+          show={modalShow}
+          onHide={() => setModalShow(false)}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+              Member Name
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>
+              Member Bio
+            </p>
+          </Modal.Body>
+        </Modal>
+      </Col>
+      <Col>
+        <Card onClick={() => {setModalShow(true)}} className="team-card">
+          <Card.Img variant="top" src={ dummyHeadshot } />
+          <Card.Body>
+            <Card.Title>Member Name</Card.Title>
+            <Card.Text>
+              Superhero Name or some catchphrase
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Modal
+          show={modalShow}
+          onHide={() => setModalShow(false)}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+              Member Name
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>
+              Member Bio
+            </p>
+          </Modal.Body>
+        </Modal>
+      </Col>
+      <Col>
+        <Card onClick={() => {setModalShow(true)}} className="team-card">
+          <Card.Img variant="top" src={ dummyHeadshot } />
+          <Card.Body>
+            <Card.Title>Member Name</Card.Title>
+            <Card.Text>
+              Superhero Name or some catchphrase
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Modal
+          show={modalShow}
+          onHide={() => setModalShow(false)}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+              Member Name
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>
+              Member Bio
+            </p>
+          </Modal.Body>
+        </Modal>
+      </Col>
     </Row>
     <Row xs={1} md={2} lg={4} className="g-4 team-row">
+        <Col>
+        <Card onClick={() => {setModalShow(true)}} className="team-card">
+          <Card.Img variant="top" src={ dummyHeadshot } />
+          <Card.Body>
+            <Card.Title>Member Name</Card.Title>
+            <Card.Text>
+              Superhero Name or some catchphrase
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Modal
+          show={modalShow}
+          onHide={() => setModalShow(false)}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+              Member Name
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>
+              Member Bio
+            </p>
+          </Modal.Body>
+        </Modal>
+      </Col>
       <Col>
-      <Card onClick={() => {setModalShow(true)}} className="team-card">
-        <Card.Img variant="top" src={ dummyHeadshot } />
-        <Card.Body>
-          <Card.Title>Member Name</Card.Title>
-          <Card.Text>
-            Superhero Name or some catchphrase
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Modal
-        show={modalShow}
-        onHide={() => setModalShow(false)}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            Member Name
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <p>
-            Member Bio
-          </p>
-        </Modal.Body>
-      </Modal>
-    </Col>
-    <Col>
-      <Card onClick={() => {setModalShow(true)}} className="team-card">
-        <Card.Img variant="top" src={ dummyHeadshot } />
-        <Card.Body>
-          <Card.Title>Member Name</Card.Title>
-          <Card.Text>
-            Superhero Name or some catchphrase
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Modal
-        show={modalShow}
-        onHide={() => setModalShow(false)}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            Member Name
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <p>
-            Member Bio
-          </p>
-        </Modal.Body>
-      </Modal>
-    </Col>
-    <Col>
-      <Card onClick={() => {setModalShow(true)}} className="team-card">
-        <Card.Img variant="top" src={ dummyHeadshot } />
-        <Card.Body>
-          <Card.Title>Member Name</Card.Title>
-          <Card.Text>
-            Superhero Name or some catchphrase
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Modal
-        show={modalShow}
-        onHide={() => setModalShow(false)}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            Member Name
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <p>
-            Member Bio
-          </p>
-        </Modal.Body>
-      </Modal>
-    </Col>
-    <Col>
-      <Card onClick={() => {setModalShow(true)}} className="team-card">
-        <Card.Img variant="top" src={ dummyHeadshot } />
-        <Card.Body>
-          <Card.Title>Member Name</Card.Title>
-          <Card.Text>
-            Superhero Name or some catchphrase
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Modal
-        show={modalShow}
-        onHide={() => setModalShow(false)}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            Member Name
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <p>
-            Member Bio
-          </p>
-        </Modal.Body>
-      </Modal>
-    </Col>
+        <Card onClick={() => {setModalShow(true)}} className="team-card">
+          <Card.Img variant="top" src={ dummyHeadshot } />
+          <Card.Body>
+            <Card.Title>Member Name</Card.Title>
+            <Card.Text>
+              Superhero Name or some catchphrase
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Modal
+          show={modalShow}
+          onHide={() => setModalShow(false)}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+              Member Name
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>
+              Member Bio
+            </p>
+          </Modal.Body>
+        </Modal>
+      </Col>
+      <Col>
+        <Card onClick={() => {setModalShow(true)}} className="team-card">
+          <Card.Img variant="top" src={ dummyHeadshot } />
+          <Card.Body>
+            <Card.Title>Member Name</Card.Title>
+            <Card.Text>
+              Superhero Name or some catchphrase
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Modal
+          show={modalShow}
+          onHide={() => setModalShow(false)}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+              Member Name
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>
+              Member Bio
+            </p>
+          </Modal.Body>
+        </Modal>
+      </Col>
+      <Col>
+        <Card onClick={() => {setModalShow(true)}} className="team-card">
+          <Card.Img variant="top" src={ dummyHeadshot } />
+          <Card.Body>
+            <Card.Title>Member Name</Card.Title>
+            <Card.Text>
+              Superhero Name or some catchphrase
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Modal
+          show={modalShow}
+          onHide={() => setModalShow(false)}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+              Member Name
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>
+              Member Bio
+            </p>
+          </Modal.Body>
+        </Modal>
+      </Col>
     </Row>
     </Template>
   );
