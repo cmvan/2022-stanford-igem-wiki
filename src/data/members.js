a6b8b09abba2cc2ffaf5d6d63a11608cba219b80
Caroline Van <cmvan@stanford.edu>
2022-10-09 18:30:57 -0700

    light style changes, update members info

--- src/data/members.js
+++ src/data/members.js
@@ -3,22 +3,22 @@ export const membersInfo = [
     id: 0,
     name: "Angel Luna",
     img: {
-      src: "https://static.igem.wiki/teams/4466/wiki/dummy-headshot.jpg",
+      src: "https://static.igem.wiki/teams/4466/wiki/members/angel-luna.jpg",
       alt: "Picture of Angel Luna",
     },
     catchphrase: "Angel's Catchphrase",
-    bio: "Angel's Bio",
+    bio: "Angel is a sophomore from Houston, TX planning on majoring in Bioengineering and getting a coterm in Computer Science. His previous experience mainly consists of personal coding, finance, and robotics projects. After taking IB Biology in HS, bio tech became a huge interest that he’s currently exploring. With the current iGEM team, he hopes to gain wet lab skill and web development experience. Outside of academics, Angel loves to play soccer, chess and video games. Recently he began learning how to play guitar and has taken playing chess and soccer more seriously. He loves to meet new people and make new experiences.",
     attribution: "Angel's Attribution"
   }, 
   {
     id: 1,
     name: "Caroline Van",
     img: {
-      src: "https://static.igem.wiki/teams/4466/wiki/dummy-headshot.jpg",
+      src: "https://static.igem.wiki/teams/4466/wiki/members/caroline-van.jpg",
       alt: "Picture of Caroline Van",
     },
     catchphrase: "Caroline's Catchphrase",
-    bio: "Caroline's Bio",
+    bio: "Caroline is a junior from Fredericksburg, VA who is majoring in computer science and minoring in French. Previously, she studied the strain-level dynamics following fecal microbiota transplants under Dr. Benjamin Good. Her interests range from computational biology to full stack web development and graphics design. Outside of coursework and lab, she enjoys engaging with her heritage with the Stanford Vietnamese Student Association, dancing in Stanford Mua Lac Hong, and working at the Bridge Peer Counseling Center. In her free time, she takes long bike rides and walks, compiles cute and wholesome gifs, and listens to the latest K-Pop releases.",
     attribution: "Caroline's Attribution"
   },
   {
@@ -29,40 +29,40 @@ export const membersInfo = [
       alt: "Picture of Claudia Phillips",
     },
     catchphrase: "Claudia's Catchphrase",
-    bio: "Claudia's Bio",
+    bio: "Claudia is a junior from Michigan majoring in Bioengineering, with a minor in German Studies. Previously, Claudia has done research on the metabolic and pathogenic behavior of fungi under Dr. Linda Hanson and the structure-function relationships of eye proteins under Dr. Vinit Mahajan. She is looking forward to deepening her knowledge of structural biology through hands-on protein engineering with the iGEM team this summer. When she isn’t in the lab, Claudia writes Science Olympiad exams and coaches local teams, trains and competes with the Stanford Taekwondo team, and knits hats.",
     attribution: "Claudia's Attribution"
   },
   {
     id: 3,
-    name: "Gurmen Bahia",
+    name: "Gurmenjit Bahia",
     img: {
       src: "https://static.igem.wiki/teams/4466/wiki/dummy-headshot.jpg",
-      alt: "Picture of Gurmen Bahia",
+      alt: "Picture of Gurmenjit Bahia",
     },
-    catchphrase: "Gurmen's Catchphrase",
-    bio: "Gurmen's Bio",
-    attribution: "Gurmen's Attribution"
+    catchphrase: "Gurmenjit's Catchphrase",
+    bio: "Gurmenjit is a junior studying Biology with a concentration in biochemistry/biophysics from Modesto, CA. She particularly enjoys research revolving around genetic editing technology and healthcare disparities. Additionally, she is involved with the Stanford Premedical Association Health Policy Committee, undergraduate senate, and a group advocating for First-generation and/or low-income students known as FLIP. She aspires to become an Orthopedic surgeon and go into global surgery, while continuing to complete research. Aside from her academics, she loves nature, gardening, and long walks.",
+    attribution: "Gurmenjit's Attribution"
   },
   {
     id: 4,
-    name: "Liz Schmidt",
+    name: "Elizabeth Schmidt",
     img: {
       src: "https://static.igem.wiki/teams/4466/wiki/dummy-headshot.jpg",
-      alt: "Picture of Liz Schmidt",
+      alt: "Picture of Elizabeth Schmidt",
     },
     catchphrase: "Liz's Catchphrase",
-    bio: "Liz's Bio",
-    attribution: "Liz's Attribution"
+    bio: "Elizabeth is a sophomore from Corpus Christi, Texas majoring in Materials Science and Engineering with a focus on Bioengineering and an intended minor in Biology. She is an undergraduate member of Hong Neurotechnology Lab currently working on projects within the intersections of nanotechnology and optogenetics. Elizabeth is passionate about interdisciplinary learning and has current academic interests spanning synthetic and systems biology, biomaterials, optogenetics, neuroengineering, and protein engineering. Outside of class and lab, Elizabeth is involved with private research projects, NASA L’SPACE, Stanford Health Consulting Group, and Stanford Space Initiative. In her free time, she loves to swim, cook, spend time with friends, and explore various beaches across California.",
+    attribution: "Elizabeth's Attribution"
   },
   {
     id: 5,
     name: "Savana Huskins",
     img: {
-      src: "https://static.igem.wiki/teams/4466/wiki/dummy-headshot.jpg",
+      src: "https://static.igem.wiki/teams/4466/wiki/members/savana-huskins.jpg",
       alt: "Picture of Savana Huskins",
     },
     catchphrase: "Savana's Catchphrase",
-    bio: "Savana's Bio",
+    bio: "Savana is a senior studying Bioengineering from Redondo Beach, CA. She is interested in plant biology, genetic engineering, and sustainable agriculture. After undergrad, she intends to pursue a PhD in bioengineering with a focus on synthetic biology, and she hopes to eventually work towards the development of biomaterials or agricultural technologies in industry. In addition to STEM courses, Savana always has a music class in her schedule and can be found playing piano and guitar in her free time. She also enjoys cooking, ocean swimming, skating around campus, and going on hikes with friends around the Bay Area.",
     attribution: "Savana's Attribution"
   },
   {
@@ -73,7 +73,7 @@ export const membersInfo = [
       alt: "Picture of Shreya Vailaya",
     },
     catchphrase: "Shreya's Catchphrase",
-    bio: "Shreya's Bio",
+    bio: "Shreya arrived at Stanford from Tokyo, Japan with a passion for engineering and music. She enjoys experimenting with DNA to unravel the intricacies of life in the same way she loves experimenting with musical notes to unveil the beauty of a song. A junior majoring in Bioengineering with a minor in Music, Shreya has conducted research in Roger Kornberg Structural Biology Laboratory at Stanford Medical School to elucidate the role of chromatin looping on gene regulation. She is passionate about exploring the interdisciplinary areas of synthetic biology, human biology, and bioinformatics for applications in medicine and environmental sustainability. This is Shreya’s second iGEM competition and she is extremely excited about this year’s research project “Be-SAFE.” Aside from academics, Shreya loves yoga, composing music, and singing with Stanford Everyday People A cappella!",
     attribution: "Shreya's Attribution"
   },
   {
@@ -84,7 +84,7 @@ export const membersInfo = [
       alt: "Picture of Sherlock Langevine",
     },
     catchphrase: "Sherlock's Catchphrase",
-    bio: "Sherlock's Bio",
+    bio: "Sherlock is a sophomore planning on majoring in Mathematical and Computational Science and minoring in Education with a Notation in Science Communication. He has a keen interest in bridging the gap between scientific research and public policy, and through iGEM he hopes to hone those skills. He believes synthetic biology is a means through which scientists can create meaningful change in the world, from the curing of diseases to the up-cycling of non-renewable resources. By being part of the Stanford iGEM team he begins his journey in partaking part of this global change.",
     attribution: "Sherlock's Attribution"
   }
 ];
