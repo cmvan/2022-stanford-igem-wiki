13b60b6a6f5491fc8d57335284f15109c577ce05
Caroline Van <cmvan@stanford.edu>
2022-10-09 18:31:24 -0700

    first draft of improve, parts pages

--- src/pages/Improve.js
+++ src/pages/Improve.js
@@ -3,7 +3,59 @@ import Template from './Template';
 export default function Improve() {
   return (
     <Template title="Improve">
-      <h1>This is the Improve Page</h1>
+      <h1>Rational Design of an Acid-Tolerant PETase Enzyme</h1>
+      <p>
+        Over the course of our project we developed several Registry parts with new or expanded 
+        functionalities. Two were FAST-PETase mutants we developed through rational design. They
+        improve upon the original PETase by adapting the enzyme to a lower functional pH, more 
+        suitable for use in yeast and other well-characterized and scalable biological chassis.
+        One of our variants, which mutated a catalytic histidine (residue number 237) to an 
+        aspartic acid, was based directly on the work of iGEM Washington in 2011 on lowering 
+        the functional pH of Kumamolisin-As. The other variant we designed used a related 
+        principle, switching a key residue to a structurally and functionally similar amino acid 
+        with a lower pKa.
+      </p>
+      <p>
+        The PETase enzyme is structurally similar to several cutinase proteins, which catalyze the
+        breakdown of plant waxes via hydrolysis. These cutinases fit into a class of enzymes called
+        serine hydrolases, as does PETase (Han et al., 2017). The active site of PETase consists of
+        a catalytic triad: a binding pocket flanked by three amino acids: serine, histidine, and 
+        aspartic acid. 
+      </p>
+      <p>
+        Based on a general understanding of the behavior of serine proteases, the mechanism for 
+        PET degradation by PETase likely takes place over three general reaction stages:
+      </p>
+      <ol>
+        <li>
+          Histidine pulls a hydrogen off of the serine, allowing the serine’s oxygen to attack the 
+          PETase carbonyl carbon. The positive charge that forms on the histidine as a result is 
+          stabilized through hydrogen bonding with the negatively charged aspartic acid.
+        </li>
+        <li>
+          The tetrahedral carbon re-forms a carbonyl while the ethyl group leaves as ethylene glycol.
+        </li>
+        <li>
+        A second water molecule binds to the carbonyl, releasing terephthalic acid as the second 
+        product and allowing the enzyme to be recycled to its original form (arrow pushing not shown).
+        </li>
+      </ol>
+      <p>
+        This type of enzyme-mediated reaction pathway is colloquially referred to as a ping-pong 
+        mechanism. Another enzyme class that commonly uses ping-pong mechanisms is cysteine 
+        hydrolases, which mirror serine hydrolases in their catalytic triads, but use a cysteine, 
+        rather than serine, as the initial nucleophile. In order to lower the functional pH of 
+        FAST-PETase, we decided to try converting it from a serine hydrolase to a cysteine 
+        hydrolase by mutating residue 160 from Ser to Cys. We hypothesized that this change would 
+        cause the protein to operate more effectively at a lower pH, because the hydrolysis 
+        reaction is dependent on the deprotonation of the nucleophile, and cysteine deprotonates 
+        more easily than serine under acidic conditions.
+      </p>
+      <p>
+        We have validated the structure of this mutant through computational modeling, available 
+        in our Project Modeling tab, and are in the process of comparing the efficiencies of the 
+        original FAST-PETase and our mutant under low pH experimentally (see our Results tab).
+      </p>
     </Template>
   );
 }
--- src/pages/Parts.js
+++ src/pages/Parts.js
@@ -1,9 +1,128 @@
+import Table from 'react-bootstrap/Table'
 import Template from './Template';
 
 export default function Parts() {
   return (
     <Template title="Parts">
-      <h1>This is the Parts Page</h1>
+      <h1>Parts</h1>
+      <Table hover responsive>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Type</th>
+            <th>Description</th>
+            <th>Designers</th>
+            <th>Length</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>BBa_K4466001</td>
+            <td>Signaling</td>
+            <td>S. cerevisiae alpha-factor secretion signal</td>
+            <td>Phillip Kyriakakis, Savana Huskins</td>
+            <td>273 bp</td>
+          </tr>
+          <tr>
+            <td>BBa_K4466002</td>
+            <td>Coding</td>
+            <td>Saccharomyces cerevisiae Codon Optimized FAST-PETase</td>
+            <td>Phillip Kyriakakis, Shreya Vailaya</td>
+            <td>792 bp</td>
+          </tr>
+          <tr>
+            <td>BBa_K4466003</td>
+            <td>Coding</td>
+            <td>FAST-PETase S160C Mutant</td>
+            <td>Claudia Phillips</td>
+            <td>792 bp</td>
+          </tr>
+          <tr>
+            <td>BBa_K4466004</td>
+            <td>Coding</td>
+            <td>FAST-PETase H237D Mutant</td>
+            <td>Claudia Phillips</td>
+            <td>792 bp</td>
+          </tr>
+          <tr>
+            <td>BBa_K4466005</td>
+            <td>Coding</td>
+            <td>Evolved FAST-PETase</td>
+            <td>Gurmenjit Bahia</td>
+            <td>792 bp</td>
+          </tr>
+          <tr>
+            <td>BBa_K4466006</td>
+            <td>Signaling</td>
+            <td>Evolved alpha-factor secretion signal</td>
+            <td>Gurmenjit Bahia</td>
+            <td>273 bp</td>
+          </tr>
+          <tr>
+            <td>BBa_K4466028</td>
+            <td>Plasmid</td>
+            <td>pAR-Ec633-trp1 (p*)</td>
+            <td>Phillip Kyriakakis, Chang Liu</td>
+            <td>7455 bp</td>
+          </tr>
+          <tr>
+            <td>BBa_K4466029</td>
+            <td>DNA</td>
+            <td>1st homology arm to integrate into OrthoRep p1</td>
+            <td>Phillip Kyriakakis</td>
+            <td>138 bp</td>
+          </tr>
+          <tr>
+            <td>BBa_K4466030</td>
+            <td>DNA</td>
+            <td>2nd homology arm to integrate into OrthoRep p1</td>
+            <td>Phillip Kyriakakis</td>
+            <td>34 bp</td>
+          </tr>
+          <tr>
+            <td>BBa_K4466017</td>
+            <td>Composite</td>
+            <td>Alpha ss + FAST-PETase</td>
+            <td>Gurmenjit Bahia, Savana Huskins, Shreya Vailaya</td>
+            <td>1073 bp</td>
+          </tr>
+          <tr>
+            <td>BBa_K4466031</td>
+            <td>Composite</td>
+            <td>Evolved Alpha ss + FAST-PETase</td>
+            <td>Gurmenjit Bahia, Savana Huskins, Shreya Vailaya</td>
+            <td>1073 bp</td>
+          </tr>
+          <tr>
+            <td>BBa_K4466022</td>
+            <td>Composite</td>
+            <td>OrthoRep integration cassette with Ura3, Leu2, mKate2, AmpR and PETase</td>
+            <td>Gurmenjit Bahia, Phillip Kyriakakis</td>
+            <td>5018 bp</td>
+          </tr>
+          <tr>
+            <td>BBa_K4466027</td>
+            <td>Composite</td>
+            <td>OrthoRep Integration Cassette with Evolved PETase</td>
+            <td>Gurmenjit Bahia, Savana Huskins</td>
+            <td>5018 bp</td>
+          </tr>
+          <tr>
+            <td>BBa_K4466032</td>
+            <td>Composite</td>
+            <td>His-SUMO tag + FAST-PETase S160C Mutant</td>
+            <td>Claudia Phillips</td>
+            <td>1123 bp</td>
+          </tr>
+          <tr>
+            <td>BBa_K4466033</td>
+            <td>Composite</td>
+            <td>His-SUMO tag + FAST-PETase H237D Mutant</td>
+            <td>Claudia Phillips</td>
+            <td>1123 bp</td>
+          </tr>
+        </tbody>
+      </Table>
     </Template>
   );
 }
