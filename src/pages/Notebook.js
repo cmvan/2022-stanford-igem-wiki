9d7084d0e932b260b7be7b2bb606f40e01927af6
Caroline Van <cmvan@stanford.edu>
2022-10-10 21:45:05 -0700

    draft of lab notebook viewer

--- src/pages/Notebook.js
+++ src/pages/Notebook.js
@@ -1,9 +1,13 @@
 import Template from './Template';
 
+// implement this if have time? https://pdfjs.express/blog/how-embed-pdf-in-html-website
+
 export default function Notebook() {
   return (
     <Template title="Notebook">
-      <h1>This is the Notebook Page</h1>
+      <h1>Wet Lab Notebook</h1>
+      <p>Check out our notebook down below for a day-by-day play of our time in the lab!</p>
+      <iframe title="2022 Stanford iGEM Wet Lab Notebook" width="100%" height="620" src="https://static.igem.wiki/teams/4466/wiki/dummy.pdf"></iframe>
     </Template>
   );
 }
