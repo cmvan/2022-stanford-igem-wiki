2529bb12caa91661846b5822ea64ad2dab87cb7f
Caroline Van <cmvan@stanford.edu>
2022-10-12 07:57:38 -0700

    sustainability write-up

--- src/pages/Sustainable.js
+++ src/pages/Sustainable.js
@@ -3,7 +3,10 @@ import Template from './Template';
 export default function Sustainable() {
   return (
     <Template title="Sustainable">
-      <h1>This is the Sustainable Page</h1>
+      <h1>Focusing on Sustainability</h1>
+      <p>
+      Our project is focused on degrading PET which is an environmental issue. By helping work towards a scalable PET degradation system, we would allow for more sustainable choices to be made by others i.e being able to use PET for things because it would be now degradable SDG #12 is responsible for consumption and production. We additionally collaborated with ASIJ with 2 surveys to gauge public knowledge/responsibility of what happens to their plastic waste. Additionally the byproducts that form as the result of using the PETase we optimized, we have found means of further degrading those through the development of a bioreactor to ensure that through the process of degrading one harmful material – PET plastic – we are not forming other harmful materials, thus, further assisting in sustainable practices. 
+      </p>
     </Template>
   );
 }
