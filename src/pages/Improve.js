2c5a37c74d39b5eee9daaaeb585c9230510561fa
Caroline Van <cmvan@stanford.edu>
2022-10-11 22:50:20 -0700

    initial draft of improve

--- src/pages/Improve.js
+++ src/pages/Improve.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import Template from './Template';
 
 export default function Improve() {
@@ -10,10 +11,11 @@ export default function Improve() {
         improve upon the original PETase by adapting the enzyme to a lower functional pH, more 
         suitable for use in yeast and other well-characterized and scalable biological chassis.
         One of our variants, which mutated a catalytic histidine (residue number 237) to an 
-        aspartic acid, was based directly on the work of iGEM Washington in 2011 on lowering 
-        the functional pH of Kumamolisin-As. The other variant we designed used a related 
-        principle, switching a key residue to a structurally and functionally similar amino acid 
-        with a lower pKa.
+        aspartic acid, was based directly on the work of 
+        <a href="https://2011.igem.org/Team:Washington/Celiacs/Background"> iGEM Washington in 
+        2011 on lowering the functional pH of Kumamolisin-As</a>. The other variant we designed 
+        used a related principle, switching a key residue to a structurally and functionally 
+        similar amino acid with a lower pKa.
       </p>
       <p>
         The PETase enzyme is structurally similar to several cutinase proteins, which catalyze the
@@ -36,8 +38,8 @@ export default function Improve() {
           The tetrahedral carbon re-forms a carbonyl while the ethyl group leaves as ethylene glycol.
         </li>
         <li>
-        A second water molecule binds to the carbonyl, releasing terephthalic acid as the second 
-        product and allowing the enzyme to be recycled to its original form (arrow pushing not shown).
+          A second water molecule binds to the carbonyl, releasing terephthalic acid as the second 
+          product and allowing the enzyme to be recycled to its original form (arrow pushing not shown).
         </li>
       </ol>
       <p>
@@ -45,7 +47,7 @@ export default function Improve() {
         mechanism. Another enzyme class that commonly uses ping-pong mechanisms is cysteine 
         hydrolases, which mirror serine hydrolases in their catalytic triads, but use a cysteine, 
         rather than serine, as the initial nucleophile. In order to lower the functional pH of 
-        FAST-PETase, we decided to try converting it from a serine hydrolase to a cysteine 
+        FAST-PETase, we decided to try converting it from a serine hydrolåase to a cysteine 
         hydrolase by mutating residue 160 from Ser to Cys. We hypothesized that this change would 
         cause the protein to operate more effectively at a lower pH, because the hydrolysis 
         reaction is dependent on the deprotonation of the nucleophile, and cysteine deprotonates 
@@ -53,8 +55,9 @@ export default function Improve() {
       </p>
       <p>
         We have validated the structure of this mutant through computational modeling, available 
-        in our Project Modeling tab, and are in the process of comparing the efficiencies of the 
-        original FAST-PETase and our mutant under low pH experimentally (see our Results tab).
+        in <Link to="/model">Project Modeling</Link>, and are in the 
+        process of comparing the efficiencies of the original FAST-PETase and our mutant under 
+        low pH experimentally (see <Link to="/results">Results</Link>).
       </p>
     </Template>
   );
