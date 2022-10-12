f418d18ca1b4faa81317abd0f15d9294d2c39c20
Caroline Van <cmvan@stanford.edu>
2022-10-12 03:30:50 -0700

    draft of inclusivity

--- src/pages/Inclusivity.js
+++ src/pages/Inclusivity.js
@@ -3,7 +3,29 @@ import Template from './Template';
 export default function Inclusivity() {
   return (
     <Template title="Inclusivity">
-      <h1>This is the Inclusivity Page</h1>
+      <h1>Promoting Inclusivity</h1>
+      <p>
+        Given iGEM's emphasis on local people solving local problems and increasing synthetic biology's accessibility, our team strived to prioritize inclusivity in our work. We accomplish this primiarly through the reach of our education program and the thoughtful design of our wiki.
+      </p>
+
+      <h2>Education Program</h2>
+      <p>
+        As a participant of an international competition, we felt it necessary to share our knowledge and contribution in scientific research to the rest of the world. Therefore, when creating our educational aspect of our program, the team emphasized on reaching as many individuals around the world as possible. Moreover, we reached out to many high school programs such as EMERGE, LEDA, and MITES which are formed by a diverse student population. Since members of our team are from different parts of the world such as Japan, Mexico, and Guyana, we were able to reach students far beyond the United States. 
+      </p>
+      <p>
+        IMAGE OF MAP
+      </p>
+      <p>
+        Overall, we had over 40 student participants partaking in our summer session. To ensure the participation of all students, we used zoom to have our sessions recorded for those students with conflicting schedules/different time zones. Our goal was to make the students feel they’re a part of a close-knit community. Therefore we regularly hosted office hours and Q&A sessions where students could ask questions and work together on assignments and overall obtain a better understanding of the concepts.
+      </p>
+      <p>
+        In order to improve our program we constantly requested feedback from the students on their thoughts and opinions. From this feedback and our experience we learned that it would be best to begin the promotion and outreach much earlier in order to increase the inclusivity. However, students enjoyed the hands-on experience and felt they had a clear communication with the team and felt comfortable participating.
+      </p>
+
+      <h2>Wiki Design</h2>
+      <p>
+        When creating any website or highly used interface, it is important to consider user friendly characteristics that make it easier for users to read, and interact with. Therefore, during the creation of our wiki, we decided to apply certain characteristics to increase our inclusivity in displaying our scientific research. For example, our Wiki uses a color blindness simulator to ensure that the wiki is accessible to individuals with color blindness. A few more intentional characteristics include the HTML tagging that allows for maximum readability via screen reader and the wiki access across multiple browsers (and their versions). Both of these characteristics increase the accessibility of our wiki and therefore the inclusivity of our research. 
+      </p>
     </Template>
   );
 }
