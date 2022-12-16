e7c14b6cfca98f9bd0d5934616f13046051fb74d
Caroline Van <cmvan@stanford.edu>
2022-12-15 23:28:41 -0500

    update wet lab notebook, edit text on hero imgs

--- src/pages/Notebook.js
+++ src/pages/Notebook.js
@@ -7,7 +7,7 @@ export default function Notebook() {
     <Template title="Notebook">
       <h1>Wet Lab Notebook</h1>
       <p>Check out our notebook down below for a day-by-day play of our time in the lab!</p>
-      <iframe title="2022 Stanford iGEM Wet Lab Notebook" width="100%" height="620" src="https://static.igem.wiki/teams/4466/wiki/pages/wet-lab-notebook.pdf"></iframe>
+      <iframe title="2022 Stanford iGEM Wet Lab Notebook" width="100%" height="620" src="https://static.igem.wiki/teams/4466/wiki/pages/updated-wet-lab-notebook.pdf"></iframe>
     </Template>
   );
 }
